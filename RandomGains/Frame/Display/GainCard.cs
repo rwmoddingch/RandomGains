61236397d8f7b09828501decc9b3b6e04817db85
Tricrystal <34974954+pkuyo@users.noreply.github.com>
2023-09-01 19:49:19 +0800

    GainCardTexture初步

--- RandomGains/Frame/Display/GainCard.cs
+++ RandomGains/Frame/Display/GainCard.cs
@@ -3,10 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RWCustom;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 namespace RandomGains.Frame
 {
-    internal class GainCard
+    public class GainCardTexture
     {
+        class GainCardImpl : MonoBehaviour
+        {
+            public GainCardImpl()
+            {
+            }
+
+            public void Update()
+            {
+                rotation += Time.deltaTime*10;
+                card.cardObject.transform.rotation = Quaternion.Euler(0, rotation,0);
+            }
+
+            public float rotation;
+            public GainCardTexture card;
+        }
+
+        public void Destroy()
+        {
+            GameObject.Destroy(cardObject);
+            GameObject.Destroy(cameraObject);
+            RenderTexture.ReleaseTemporary(Texture);
+            Texture = null;
+        }
+
+    
+
+
+        public GainCardTexture()
+        {
+            count++;
+
+            cardObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            cardObject.GetComponent<MeshRenderer>().sharedMaterial =
+                new Material(Custom.rainWorld.Shaders["Basic"].shader);
+           cameraObject = new GameObject("GainCard_Camera");
+
+            camera = cameraObject.AddComponent<Camera>();
+            
+            cameraObject.AddComponent<Transform>();
+            camera.targetTexture = Texture;
+
+            gainCard = cardObject.AddComponent<GainCardImpl>();
+            gainCard.card = this;
+            cardObject.AddComponent<Transform>();
+
+            cameraObject.transform.position = new Vector3( count * 700, -10000f, -100f);
+            cardObject.transform.position = new Vector3(count * 700, -10000f, -100f + 2f);
+            cardObject.transform.localScale = new Vector3(0.6f, 1f);
+            
+        }
+
+        public RenderTexture Texture { get; private set; } = RenderTexture.GetTemporary(500, 300);
+
+        private Camera camera;
+        private GainCardImpl gainCard;
+
+        private GameObject cardObject;
+        private GameObject cameraObject;
+
+        private static int count = 0;
     }
 }
--- RandomGains/Plugins.cs
+++ RandomGains/Plugins.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
+using RandomGains.Frame;
+using RWCustom;
 using UnityEngine;
 
 #pragma warning disable CS0618
@@ -21,6 +23,20 @@ namespace RandomGains
             On.RainWorld.OnModsInit += RainWorld_OnModsInit;
         }
 
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (Custom.rainWorld.processManager.currentMainLoop is RainWorldGame game &&
+                    game.AlivePlayers[0].realizedCreature is Player player &&
+                    player.room != null)
+                {
+                    player.room.AddObject(new Test(player));
+                    Debug.Log("[SSSS] Add Test");
+                }
+            }
+        }
+
         private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
         {
             orig.Invoke(self);
@@ -31,6 +47,7 @@ namespace RandomGains
                 CustomDeathPersistentSaveTx.DeathPersistentSaveDataRx.AppplyTreatment(new GainSave(null));
 
                 GainHookWarpper.WarpHook(new On.Player.hook_Update(Player_Update), null);//暂时写个null
+
             }
 
             catch (Exception e) 
@@ -54,5 +71,39 @@ namespace RandomGains
     public class BounceSpearGain : Gain<BounceSpearGain, BounceSpearData>
     {
 
+    }
+
+    public class Test : CosmeticSprite
+    {
+        public Test(Player player)
+        {
+            card = new GainCardTexture();
+            pos = player.DangerPos;
+        }
+
+        private Vector2 pos;
+        private GainCardTexture card;
+        public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+        {
+            base.InitiateSprites(sLeaser, rCam);
+            sLeaser.sprites = new FSprite[2];
+            sLeaser.sprites[0] = new FTexture(card.Texture){scale = 1};
+            sLeaser.sprites[1] = new FSprite("Futile_White");
+            AddToContainer(sLeaser,rCam,null);
+        }
+
+        public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
+            sLeaser.sprites[0].SetPosition(pos - camPos);
+            sLeaser.sprites[1].SetPosition(pos - camPos);
+
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+            card.Destroy();
+        }
     }
 }
