749984c04240a435d6de4cc527b1fdbae8a7ebb3
Harvie Sorroway <1934182494@qq.com>
2023-09-04 22:06:22 +0800

    添加了蓝果炸弹增益

---
+++ BuiltinGains/Duality/DangleBombGain.cs
@@ -0,0 +1,158 @@
+﻿using BuiltinGains.Positive;
+using MonoMod.Cil;
+using Noise;
+using RandomGains;
+using RandomGains.Frame.Core;
+using RandomGains.Gains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BuiltinGains.Duality
+{
+    internal class DangleBombGainEntry : GainEntry
+    {
+        public static GainID dangleBombGainID = new GainID("DangleBombGainID", true);
+        public static ConditionalWeakTable<DangleFruit, DangleBombModule> modules = new ConditionalWeakTable<DangleFruit, DangleBombModule>();
+
+        public static void HookOn()
+        {
+            On.Player.TossObject += Player_TossObject;
+            On.DangleFruit.BitByPlayer += DangleFruit_BitByPlayer;
+            On.DangleFruit.Update += DangleFruit_Update;
+        }
+
+        private static void Player_TossObject(On.Player.orig_TossObject orig, Player self, int grasp, bool eu)
+        {
+            if (self.grasps[grasp] != null && self.grasps[grasp].grabbed is DangleFruit dangleFruit)
+            {
+                if (modules.TryGetValue(dangleFruit, out var module))
+                    module.bombActivate = true;
+            }
+            orig.Invoke(self, grasp, eu);
+        }
+
+        private static void DangleFruit_Update(On.DangleFruit.orig_Update orig, DangleFruit self, bool eu)
+        {
+            orig.Invoke(self, eu);
+            if (modules.TryGetValue(self, out var module))
+            {
+                module.Update(self);
+            }
+            else
+                modules.Add(self, new DangleBombModule(self));
+        }
+
+        private static void DangleFruit_BitByPlayer(On.DangleFruit.orig_BitByPlayer orig, DangleFruit self, Creature.Grasp grasp, bool eu)
+        {
+            orig.Invoke(self, grasp, eu);
+            if(modules.TryGetValue(self, out var module))
+            {
+                module.BombmaawaExclamationExclamationExclamation(self);
+            }
+        }
+
+        public override void OnEnable()
+        {
+            GainRegister.RegisterGain<DangleBombGainImpl, DangleBombGainDataImpl, DangleBombGainEntry>(dangleBombGainID);
+        }
+    }
+
+    internal class DangleBombGainDataImpl : GainDataImpl
+    {
+        int cycleLeft;
+
+        public override GainID GainID => DangleBombGainEntry.dangleBombGainID;
+
+        public override void Init()
+        {
+            EmgTxCustom.Log($"DangleBombGainDataImpl : init");
+            base.Init();
+            cycleLeft = 2;
+        }
+
+        public override void ParseData(string data)
+        {
+            EmgTxCustom.Log($"DangleBombGainDataImpl : Parse data : {data}");
+            cycleLeft = int.Parse(data);
+        }
+
+        public override bool SteppingCycle()
+        {
+            EmgTxCustom.Log($"DangleBombGainDataImpl : stepping cycle {cycleLeft}->{cycleLeft - 1}");
+            cycleLeft--;
+
+            return cycleLeft <= 0;
+        }
+
+        public override string ToString()
+        {
+            return cycleLeft.ToString();
+        }
+    }
+
+    internal class DangleBombGainImpl : GainImpl<DangleBombGainImpl, DangleBombGainDataImpl>
+    {
+        public override GainID ID => DangleBombGainEntry.dangleBombGainID;
+    }
+
+    internal class DangleBombModule
+    {
+        public WeakReference<DangleFruit> dangleRef;
+        Color origCol;
+        public WeakReference<Creature> lastThrowBy = new WeakReference<Creature>(null);
+
+        public bool bombActivate;
+        int bombCounter = 120;
+
+        Color CurrentColor
+        {
+            get
+            {
+                int span = bombCounter > 60 ? 20 : 10;
+                if (bombCounter % span > span / 2f)
+                    return origCol;
+                return new Color(1f, 0f, 1f);
+            }
+        }
+
+        public DangleBombModule(DangleFruit dangleFruit)
+        {
+            dangleRef = new WeakReference<DangleFruit>(dangleFruit);
+            origCol = dangleFruit.color;
+        }
+
+        public void Update(DangleFruit dangleFruit)
+        {
+            if (!bombActivate)
+                return;
+            if (bombCounter > 0)
+                bombCounter--;
+            else
+                BombmaawaExclamationExclamationExclamation(dangleFruit);
+
+            dangleFruit.color = CurrentColor;
+        }
+
+        public void BombmaawaExclamationExclamationExclamation(DangleFruit self)
+        {
+            Vector2 vector = Vector2.Lerp(self.firstChunk.pos, self.firstChunk.lastPos, 0.35f);
+            lastThrowBy.TryGetTarget(out var lastThrow);
+            self.room.AddObject(new Explosion(self.room, self, vector, 7, 250f, 6.2f, 2f, 280f, 0.25f, lastThrow, 0.7f, 160f, 1f));
+            self.room.AddObject(new SootMark(self.room, vector, 80f, true));
+
+            self.room.AddObject(new Explosion.ExplosionLight(vector, 280f, 1f, 7, origCol));
+            self.room.AddObject(new Explosion.ExplosionLight(vector, 230f, 1f, 3, new Color(1f, 1f, 1f)));
+            self.room.AddObject(new ExplosionSpikes(self.room, vector, 14, 30f, 9f, 7f, 170f, origCol));
+            self.room.AddObject(new ShockWave(vector, 330f, 0.045f, 5, false));
+
+            self.room.PlaySound(SoundID.Bomb_Explode, vector);
+            self.room.InGameNoise(new InGameNoise(vector, 9000f, self, 1f));
+            self.Destroy();
+        }
+    }
+}
--- BuiltinGains/Positive/BounceSpearGain.cs
+++ BuiltinGains/Positive/BounceSpearGain.cs
@@ -10,10 +10,14 @@ using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using UnityEngine;
 using System.Security.Permissions;
+using System.Runtime.CompilerServices;
+using RWCustom;
+using RandomGains;
+using RandomGains.Gains;
 #pragma warning disable CS0618
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
-namespace RandomGains.Gains
+namespace BuiltinGains.Positive
 {
     internal class BounceSpearGainData : GainDataImpl
     {
@@ -60,29 +64,40 @@ namespace RandomGains.Gains
     internal class BounceSpearGainHooks : GainEntry
     {
         public static GainID bounceSpearID = new GainID("BounceSpear", true);
+        public static ConditionalWeakTable<Spear, BounceSpearModule> modules = new ConditionalWeakTable<Spear, BounceSpearModule>();
 
         public static void HookOn()
         {
             On.Spear.LodgeInCreature += Spear_LodgeInCreature;
-            IL.Player.Update += Player_Update;
-            Hook newHook = new Hook(typeof(Player).GetProperty("isRivulet").GetGetMethod(),
-                typeof(BounceSpearGainHooks).GetMethod("Player_IsRiv", BindingFlags.Static | BindingFlags.NonPublic));
+            On.Spear.PickedUp += Spear_PickedUp;
+            On.Spear.Update += Spear_Update;
         }
 
-        private static bool Player_IsRiv(Func<Player, bool> orig, Player self)
+        private static void Spear_Update(On.Spear.orig_Update orig, Spear self, bool eu)
         {
-            return true;
+            orig.Invoke(self, eu);
+            if (!modules.TryGetValue(self, out var module) && !(self is ExplosiveSpear))
+            {
+                modules.Add(self, new BounceSpearModule(self));
+            }
         }
 
-        private static void Player_Update(MonoMod.Cil.ILContext il)
+        private static void Spear_PickedUp(On.Spear.orig_PickedUp orig, Spear self, Creature upPicker)
         {
-            ILCursor c = new ILCursor(il);
-            c.EmitDelegate<Action>(() => Debug.Log("Sdsdsd"));
+            orig.Invoke(self, upPicker);
+
+            if (!modules.TryGetValue(self, out var module))
+                return;
+
+            module.PickedUp();
         }
 
         private static void Spear_LodgeInCreature(On.Spear.orig_LodgeInCreature orig, Spear self, SharedPhysics.CollisionResult result, bool eu)
         {
             orig.Invoke(self, result, eu);
+
+            if (!modules.TryGetValue(self, out var module))
+                return;
 
             Vector2 vector = Vector2.Lerp(self.firstChunk.pos, self.firstChunk.lastPos, 0.35f);
             self.room.AddObject(new SootMark(self.room, vector, 80f, true));
@@ -94,11 +109,71 @@ namespace RandomGains.Gains
 
             self.room.PlaySound(SoundID.Bomb_Explode, vector);
             self.room.InGameNoise(new InGameNoise(vector, 9000f, self, 1f));
+
+            module.ShootNextTarget(self);
         }
 
         public override void OnEnable()
         {
             GainRegister.RegisterGain<BounceSpearGain, BounceSpearGainData, BounceSpearGainHooks>(bounceSpearID);
+        }
+    }
+
+    internal class BounceSpearModule
+    {
+        public WeakReference<Spear> spearRef;
+
+        public List<AbstractCreature> ignoreList = new List<AbstractCreature>();
+
+        public BounceSpearModule(Spear spear)
+        {
+            spearRef = new WeakReference<Spear>(spear);
+        }
+
+        public void PickedUp()
+        {
+            ignoreList.Clear();
+        }
+
+        public void ShootNextTarget(Spear self)
+        {
+            if (!(self.stuckInObject is Creature stuckIn))
+            {
+                return;
+            }
+            ignoreList.Add(stuckIn.abstractCreature);
+
+            var lst = from target in self.room.abstractRoom.creatures
+                      where !ignoreList.Contains(target)
+                      where target.realizedCreature != null
+                      where !(target.realizedCreature is Player)
+                      select target.realizedCreature;
+
+            float minDist = float.MaxValue;
+            Creature nextTarget = null;
+
+            foreach (var creature in lst)
+            {
+                float dist = Vector2.Distance(self.firstChunk.pos, creature.DangerPos);
+                if (dist < minDist)
+                {
+                    nextTarget = creature;
+                    minDist = dist;
+                }
+            }
+
+            if (nextTarget == null)
+                return;
+
+            Vector2 dir = (nextTarget.DangerPos - self.firstChunk.pos).normalized;
+
+            self.ChangeMode(Weapon.Mode.Free);
+            self.Thrown(self.thrownBy, self.firstChunk.pos, self.firstChunk.pos, new IntVector2(dir.x > 0 ? 1 : -1, 0), 1.5f, true);
+
+            float vel = self.firstChunk.vel.magnitude;
+            self.firstChunk.vel = vel * dir;
+            self.rotation = dir;
+            self.setRotation = dir;
         }
     }
 }
